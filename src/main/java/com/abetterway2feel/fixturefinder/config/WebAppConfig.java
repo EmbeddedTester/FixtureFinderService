3855d7bb969b144fe6565711be58396bb3842ce7
Philip Quinn <philip.a.quinn@hotmail.co.uk>
2017-04-23 00:08:12 +0100

    jsonp support

--- src/main/java/com/abetterway2feel/fixturefinder/config/WebAppConfig.java
+++ src/main/java/com/abetterway2feel/fixturefinder/config/WebAppConfig.java
@@ -1,12 +1,21 @@
 package com.abetterway2feel.fixturefinder.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
 
 @Configuration
 public class WebAppConfig extends WebMvcConfigurerAdapter {
+
+    @ControllerAdvice
+    static class JsonpAdvice extends AbstractJsonpResponseBodyAdvice {
+        public JsonpAdvice() {
+            super("callback");
+        }
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
